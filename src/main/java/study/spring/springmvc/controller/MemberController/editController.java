27eb5b962951b1c3e15bec14bddbc69f2411b771
Jae Joon <dkansk924@naver.com>
2020-11-02 23:36:42 +0900

    Session 값 HttpSession 에서 직접 가져오는 부분 - > @SessionAttribute 이용하여 타입 캐스팅 코드  줄임.

--- src/main/java/study/spring/springmvc/controller/MemberController/editController.java
+++ src/main/java/study/spring/springmvc/controller/MemberController/editController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import study.spring.springmvc.dto.member.Member;
 import study.spring.springmvc.service.memberService.MemberService;
 
@@ -21,16 +22,16 @@ public class editController {
     }
 
     @GetMapping("/memberEdit")
-    public String showEditPage(HttpSession session , Model model){
-        Long DB_id = (Long) session.getAttribute("UserDB_id");
+    public String showEditPage(@SessionAttribute("UserDB_id") Long DB_id , Model model){
+//        Long DB_id = (Long) session.getAttribute("UserDB_id");
         Member member = service.getMember(DB_id);
         model.addAttribute("member",member);
         return "/Member/edit/MemberDetailPage";
     }
 
     @PostMapping("/editMember")
-    public String editMember(@RequestParam String pw, HttpSession session){
-        Long DB_id = (Long) session.getAttribute("UserDB_id");
+    public String editMember(@RequestParam String pw, @SessionAttribute("UserDB_id") Long DB_id){
+//        Long DB_id = (Long) session.getAttribute("UserDB_id");
         Member member = service.getMember(DB_id);
         member.setPw(pw);
         service.editMember(member,DB_id);
