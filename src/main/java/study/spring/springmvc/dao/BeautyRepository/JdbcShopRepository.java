6412a2b5e16ed63139f62e94f6a69f2e0f7a0421
Jae Joon <dkansk924@naver.com>
2020-11-07 23:24:09 +0900

    디자이너 저장 구현 -> DB설계 생각 해봐야됨

--- src/main/java/study/spring/springmvc/dao/BeautyRepository/JdbcShopRepository.java
+++ src/main/java/study/spring/springmvc/dao/BeautyRepository/JdbcShopRepository.java
@@ -1,17 +1,36 @@
 package study.spring.springmvc.dao.BeautyRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import study.spring.springmvc.dto.beautyShop.BeautyShop;
 import study.spring.springmvc.dto.beautyShop.Designer;
 import study.spring.springmvc.dto.beautyShop.Menu;
 
+import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcShopRepository implements BeautyShopRepository {
+    private final JdbcTemplate template;
+
+    @Autowired
+    public JdbcShopRepository(DataSource dataSource) {
+        template = new JdbcTemplate(dataSource);
+    }
+
     @Override
     public void designerSave(Designer designer) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(template);
+        jdbcInsert.withTableName("designer").usingGeneratedKeyColumns("id");
 
+        Map<String,Object> parameters = new HashMap<>();
+        parameters.put("name",designer.getName());
+        parameters.put("specialty",designer.getSpecialty());
+        parameters.put("beautyShop_id",designer.getBeautyShop().getDB_Id());
     }
 
     @Override
